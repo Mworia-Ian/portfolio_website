4448be854ae3a0ce9c880c07e77e63cdbcd64ece
Ian Mworia <ianmworia156@gmail.com>
2024-05-16 20:51:44 +0300

    Added logos to Tech Component

--- src/components/Technologies.jsx
+++ src/components/Technologies.jsx
@@ -1,11 +1,32 @@
-import React from 'react'
+import { RiReactjsLine } from "react-icons/ri";
+import { RiJavascriptFill } from "react-icons/ri"
+import { BiLogoPostgresql } from "react-icons/bi"
+import { FaPython } from "react-icons/fa6"
+import { BiLogoFlask } from "react-icons/bi"
 
 const Technologies = () => {
   return (
-    <div>
-      
+    <div className="border-b border-neutral-800 pb-24">
+      <h1 className="my-20 text-center text-4xl">Technologies</h1>
+      <div className="flex flex-wrap items-center justify-center gap-4">
+        <div className="rounded-2xl border-4 border-neutral-800 p-4">
+          <RiReactjsLine className="text-7xl text-cyan-500" />
+        </div>
+        <div className="rounded-2xl border-4 border-neutral-800 p-4">
+          <RiJavascriptFill className="text-7xl text-yellow-500" />
+        </div>
+        <div className="rounded-2xl border-4 border-neutral-800 p-4">
+          <FaPython className="text-7xl text-cyan-500" />
+        </div>
+        <div className="rounded-2xl border-4 border-neutral-800 p-4">
+          <BiLogoPostgresql className="text-7xl text-blue-500" />
+        </div>
+        <div className="rounded-2xl border-4 border-neutral-800 p-4">
+          <BiLogoFlask className="text-7xl text-green-500" />
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Technologies
+export default Technologies;
