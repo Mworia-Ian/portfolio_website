9df3a2ed0bb33eb95ee1efb89993019a1afb467c
Ian Mworia <ianmworia156@gmail.com>
2024-05-18 01:08:59 +0300

    Added links to icons

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -4,6 +4,8 @@ import { FaGithub } from "react-icons/fa"
 import { FaSquareXTwitter } from "react-icons/fa6"
 import { FaInstagram } from "react-icons/fa"
 
+
+
 const NavBar = () => {
   return (
    <nav className="mb-20 flex items-center justify-between py-6">
@@ -15,10 +17,14 @@ const NavBar = () => {
         />
     </div>
     <div className="m-8 flex items-center justify-center gap-4 text-2xl" >
-        <FaLinkedin />
-        <FaGithub />
-        <FaSquareXTwitter />
-        <FaInstagram />
+      <a href="https://www.linkedin.com/in/ian-mworia/" target="_blank">
+      <FaLinkedin />
+      </a>
+      <a href="https://github.com/Mworia-Ian?tab=repositories" target="_blank">
+      <FaGithub />
+      </a>
+        {/* <FaSquareXTwitter />
+        <FaInstagram /> */}
     </div>
 
    </nav>
--- src/constants/index.js
+++ src/constants/index.js
@@ -1,72 +1,41 @@
 import project1 from "../assets/projects/project-1.jpg";
-import project2 from "../assets/projects/project-2.jpg";
 import project3 from "../assets/projects/project-3.jpg";
-import project4 from "../assets/projects/project-4.jpg";
 
-export const ABOUT_CONTENT = `I am a passionate full stack developer with a knack for crafting robust and scalable web applications. With 5 years of hands-on experience, I have honed my skills in front-end technologies like React and Next.js, as well as back-end technologies like Node.js, MySQL, PostgreSQL, and MongoDB. My goal is to leverage my expertise to create innovative solutions that drive business growth and deliver exceptional user experiences.`;
+export const ABOUT_CONTENT = `I am a passionate full stack developer with a knack for crafting robust and scalable web applications. I have honed my skills in front-end technologies like React and Next.js, as well as back-end technologies like Python, MySQL & PostgreSQL. My goal is to leverage my expertise to create innovative solutions that drive business growth and deliver exceptional user experiences.`;
 
-export const ABOUT_TEXT = `I am a dedicated and versatile full stack developer with a passion for creating efficient and user-friendly web applications. With 5 years of professional experience, I have worked with a variety of technologies, including React, Next.js, Node.js, MySQL, PostgreSQL, and MongoDB. My journey in web development began with a deep curiosity for how things work, and it has evolved into a career where I continuously strive to learn and adapt to new challenges. I thrive in collaborative environments and enjoy solving complex problems to deliver high-quality solutions. Outside of coding, I enjoy staying active, exploring new technologies, and contributing to open-source projects.`;
+export const ABOUT_TEXT = `I'm a passionate coder with a love for creating innovative solutions through technology. I'm captivated by the endless possibilities it offers. I specialize in software engineering, and I'm always eager to learn and explore new technologies.
+
+When I'm not coding, you'll find me on the football field or cricket pitch, enjoying the thrill and camaraderie of team sports. I'm an avid gamer, always on the lookout for the next great game to immerse myself in.
+
+Beyond my personal interests, I believe in giving back to the community. I actively participate in charity work, striving to make a positive impact wherever I can. One cause that is particularly close to my heart is rhino conservation. I am dedicated to supporting efforts to protect these magnificent creatures and ensure their survival for future generations.
+
+Thanks for visiting my portfolio! Feel free to explore my projects and get in touch if you'd like to collaborate.`;
 
 export const EXPERIENCES = [
   {
-    year: "2023 - Present",
-    role: "Senior Full Stack Developer",
-    company: "Google Inc.",
-    description: `Led a team in developing and maintaining web applications using JavaScript, React.js, and Node.js. Implemented RESTful APIs and integrated with MongoDB databases. Collaborated with stakeholders to define project requirements and timelines.`,
-    technologies: ["Javascript", "React.js", "Next.js", "mongoDB"],
-  },
-  {
-    year: "2022 - 2023",
-    role: "Frontend Developer",
-    company: "Adobe",
-    description: `Designed and developed user interfaces for web applications using Next.js and React. Worked closely with backend developers to integrate frontend components with Node.js APIs. Implemented responsive designs and optimized frontend performance.`,
-    technologies: ["HTML", "CSS", "Vue.js", "mySQL"],
-  },
-  {
-    year: "2021 - 2022",
+    year: "2024 - Present",
     role: "Full Stack Developer",
-    company: "Facebook",
-    description: `Developed and maintained web applications using JavaScript, React.js, and Node.js. Designed and implemented RESTful APIs for data communication. Collaborated with cross-functional teams to deliver high-quality software products on schedule.`,
-    technologies: ["Python", "Svelte", "Three.js", "Postgres"],
-  },
-  {
-    year: "2020 - 2021",
-    role: "Software Engineer",
-    company: "Paypal",
-    description: `Contributed to the development of web applications using JavaScript, React.js, and Node.js. Managed databases and implemented data storage solutions using MongoDB. Worked closely with product managers to prioritize features and enhancements.`,
-    technologies: ["Ruby", "Rails", "PHP", "Sqlite"],
-  },
+    company: "Moringa School",
+    description: `Led a team in developing and maintaining web applications using JavaScript, React.js, and Flask. Implemented RESTful APIs and integrated with Postgres databases. Collaborated with stakeholders to define project requirements and timelines.`,
+    technologies: ["Javascript", "React.js", "Python", "Flask", "Postgres"],
+  }
 ];
 
 export const PROJECTS = [
   {
-    title: "E-Commerce Website",
+    title: "Recipe Website",
     image: project1,
     description:
-      "A fully functional e-commerce website with features like product listing, shopping cart, and user authentication.",
-    technologies: ["HTML", "CSS", "React", "Node.js", "MongoDB"],
-  },
-  {
-    title: "Task Management App",
-    image: project2,
-    description:
-      "An application for managing tasks and projects, with features such as task creation, assignment, and progress tracking.",
-    technologies: ["HTML", "CSS", "Angular", "Firebase"],
+      "A fully functional recipe website with features like search, add to favorites and remove from favorites",
+    technologies: ["HTML", "CSS", "React", "Redux"],
   },
   {
     title: "Portfolio Website",
     image: project3,
     description:
       "A personal portfolio website showcasing projects, skills, and contact information.",
-    technologies: ["HTML", "CSS", "React", "Bootstrap"],
-  },
-  {
-    title: "Blogging Platform",
-    image: project4,
-    description:
-      "A platform for creating and publishing blog posts, with features like rich text editing, commenting, and user profiles.",
-    technologies: ["HTML", "CSS", "Vue.js", "Express", "mySQL"],
-  },
+    technologies: ["HTML", "CSS", "React", "TailwindCss"],
+  }
 ];
 
 export const CONTACT = {
