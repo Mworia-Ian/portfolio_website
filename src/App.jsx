00d7901ce1be8cf357500a8b98526db0e8c7a2bd
Ian Mworia <ianmworia156@gmail.com>
2024-05-17 20:05:35 +0300

    finished the Contacts component

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import AboutMe from "./components/AboutMe";
 import Technologies from "./components/Technologies";
 import Experience from "./components/Experience";
 import Projects from "./components/Projects";
+import Contact from "./components/Contact";
 
 function App() {
   return (
@@ -19,6 +20,7 @@ function App() {
         <Technologies />
         <Experience />
         <Projects />
+        <Contact />
       </div>
     </div>
   );
---
+++ src/components/Contact.jsx
@@ -0,0 +1,17 @@
+import { CONTACT } from "../constants"
+
+const Contact = () => {
+  return (
+    <div className="border-b border-neutral-900 pb-4">
+        <h2 className="my-20 text-center text-4xl">Contacts</h2>
+      <div className="text-center tracking-tighter">
+        <p className="my-4">{CONTACT.address}</p>
+        <p className="my-4">{CONTACT.phoneNo}</p>
+        <a href="#" className="my-4">{CONTACT.email}</a>
+      
+      </div>
+    </div>
+  )
+}
+
+export default Contact
