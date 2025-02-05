f5b65ac2b0ac9e35de326a059e90ed1c3fac5c60
ianmmworia <ianmworia156@gmail.com>
2025-02-05 23:29:23 +0300

    implement cal

--- src/components/Cal.jsx
+++ src/components/Cal.jsx
@@ -1,11 +1,14 @@
 import { getCalApi } from "@calcom/embed-react";
-  import { useEffect } from "react";
-  export default function MyApp() {
-	useEffect(()=>{
-	  (async function () {
-		const cal = await getCalApi({"namespace":"check-in"});
-		cal("floatingButton", {"calLink":"ian-mworia/check-in","config":{"layout":"month_view"}});
-		cal("ui", {"hideEventTypeDetails":false,"layout":"month_view"});
-	  })();
-	}, [])
-  }
+import { useEffect } from "react";
+export default function MyApp() {
+  useEffect(() => {
+    (async function () {
+      const cal = await getCalApi({ namespace: "check-in" });
+      cal("floatingButton", {
+        calLink: "ian-mworia/check-in",
+        config: { layout: "month_view" },
+      });
+      cal("ui", { hideEventTypeDetails: false, layout: "month_view" });
+    })();
+  }, []);
+}
